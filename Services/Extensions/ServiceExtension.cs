c5a5872c5f8314a436f3e18d957e1ada18eb208f
ensar@kkuzey <ensarakkuzey@proton.me>
2025-07-21 23:28:49 +0300

    Add project files.

---
+++ Services/Extensions/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using App.Services.Products;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Services.Extensions;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProductService, ProductService>();
+        return services;
+    }
+}
---
+++ Services/ServiceResult.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace App.Services;
+
+public class ServiceResult<T>
+{
+    public T? Data { get; set; }
+    public List<string>? Errors { get; set; }
+    public bool IsSuccess => Errors == null || Errors.Count == 0;
+    public bool IsFailure => !IsSuccess;
+    public HttpStatusCode StatusCode { get; set; }
+
+    public static ServiceResult<T> Success(T data, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        return new ServiceResult<T>
+        {
+            Data = data,
+            StatusCode = statusCode
+        };
+    }
+
+    public static ServiceResult<T> Failure(List<string> errors, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult<T>
+        {
+            Errors = errors,
+            StatusCode = statusCode
+        };
+    }
+
+    public static ServiceResult<T> Failure(string error, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult<T>
+        {
+            Errors = new List<string> { error },
+            StatusCode = statusCode
+        };
+    }
+
+}
